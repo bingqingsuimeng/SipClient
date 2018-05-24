acf2b88ff5929404d4bc1ecd71e1d656e0e72d38
YangDong <1021818992@qq.com>
2018-05-24 14:40:37 +0800

    modify git repo

--- SipClient/SipClientidl.h
+++ SipClient/SipClientidl.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Thu May 24 14:29:32 2018
+/* at Thu May 24 14:37:00 2018
  */
 /* Compiler settings for SipClient.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0603 
